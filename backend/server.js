de9316de3b4381a077587c3533be6b7f1b4d89a2
Animesh Karmakar <animeshkarmakar992@gmail.com>
2025-07-18 16:04:58 +0530

    render

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,6 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
   origin: [
-    'https://communet-nu.vercel.app',
     'http://localhost:5173' , // if using Vite
   ],
   credentials: true
